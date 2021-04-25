c9e925e8d6c8d1c87997e0c6a75c6bc758ad6168
liujuan <jane_liu900@163.com>
2021-04-25 10:07:19 +0800

    图片地址恢复

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <!-- Page Header -->
-    <header class="masthead"  :style="{ backgroundImage: `url(${process.env.GRIDSOME_API_URL + general.cover.url})` }">
+    <header class="masthead"  :style="{ backgroundImage: `url(${GRIDSOME_API_URL + general.cover.url})` }">
       <div class="overlay"></div>
       <div class="container">
         <div class="row">
